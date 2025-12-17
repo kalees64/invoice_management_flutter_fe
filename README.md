543024a02c6c103557e924bd25f22b1d81d6454b
Kaleeswaran V <130140816+kalees64@users.noreply.github.com>
2025-12-17 07:05:18 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# invoice_management_flutter_fe
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/invoice_management_flutter_fe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.invoice_management_flutter_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
